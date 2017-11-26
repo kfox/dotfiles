fcfc0508c98f77231a3af3f960bb8f74f4ae682d
Kelly Fox <kelly@bumfuddled.com>
2017-11-26 09:59:20 -0600

    make errors less noisy and stop resetting the background image

--- bin/bing-wallpaper.sh
+++ bin/bing-wallpaper.sh
@@ -4,7 +4,7 @@ PICTURE_DIR="$HOME/Pictures/Bing"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://www.bing.com | \
+urls=( $(curl -s http://www.bing.com 2>/dev/null | \
   grep -Eo "url:'.*?'" | \
   sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
   sed -e "s/\\\//g") )
@@ -14,6 +14,6 @@ for p in ${urls[@]}; do
   fullpath=$PICTURE_DIR/$filename
   if [ ! -f "${fullpath}" ]; then
     curl -s -Lo "$fullpath" $p
+    /usr/bin/osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$fullpath\""
   fi
-  /usr/bin/osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$fullpath\""
 done
