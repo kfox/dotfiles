6b394b6cb04a52529e801096eaff75b7fb54a123
Kelly Fox <kelly@bumfuddled.com>
2017-12-29 11:05:02 -0600

    use built-in support for bash-git-prompt customization

---
+++ .git-prompt-colors.sh
@@ -0,0 +1,22 @@
+override_git_prompt_colors() {
+  GIT_PROMPT_THEME_NAME="Custom"
+
+  GIT_PROMPT_MASTER_BRANCH="${Yellow}"
+  GIT_PROMPT_UPSTREAM=" (${DimBlue}_UPSTREAM_${ResetColor})"
+
+  GIT_PROMPT_START=$MAINPROMPT
+  GIT_PROMPT_END="\n$ "
+
+  GIT_PROMPT_STAGED=" ${Yellow}●${DimWhite}·${ResetColor}"
+  GIT_PROMPT_CHANGED=" ${Blue}Δ${DimWhite}·${ResetColor}"
+  GIT_PROMPT_CONFLICTS=" ${Red}✖${DimWhite}·${ResetColor}"
+  GIT_PROMPT_UNTRACKED=" ${BoldCyan}?${DimWhite}·${ResetColor}"
+  GIT_PROMPT_STASHED=" ${BoldMagenta}⚑${DimWhite}·${ResetColor}"
+  GIT_PROMPT_CLEAN=" ${Green}✔${ResetColor}"
+  GIT_PROMPT_SEPARATOR=" ${DimWhite}»${ResetColor}"
+  GIT_PROMPT_PREFIX="${DimWhite}git:{${ResetColor} "
+  GIT_PROMPT_SUFFIX=" ${DimWhite}}${ResetColor}"
+  GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="${BrightYellow}✭${ResetColor}"
+}
+
+reload_git_prompt_colors "Custom"
