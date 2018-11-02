9f0f255f1a94279630d283190978fa46ba856bf7
Kelly Fox <kelly@bumfuddled.com>
2018-11-02 16:18:31 -0500

    fix quoting and change path

--- bin/bing-wallpaper.sh
+++ bin/bing-wallpaper.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-PICTURE_DIR="$HOME/Pictures/Bing"
+PICTURE_DIR="$HOME/Google Drive/Pictures/Bing"
 
-mkdir -p $PICTURE_DIR
+mkdir -p "$PICTURE_DIR"
 
-urls=( $(curl -s http://www.bing.com 2>/dev/null | \
+urls=( "$(curl -s http://www.bing.com 2>/dev/null | \
   grep -Eo "url:'.*?'" | \
-  sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
-  sed -e "s/\\\//g") )
+  sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" -e "s/\\\//g")" )
 
-for p in ${urls[@]}; do
-  filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
+
+for p in "${urls[@]}"; do
+  filename=$(echo $p | sed 's/.*\/\(.*\)/\1/')
   fullpath=$PICTURE_DIR/$filename
   if [ ! -f "${fullpath}" ]; then
-    curl -s -Lo "$fullpath" $p
+    curl -s -Lo "$fullpath" "$p"
     /usr/bin/osascript -e \
       "tell application \"Finder\" to set desktop picture to POSIX file \"$fullpath\"" \
       >/dev/null 2>&1
