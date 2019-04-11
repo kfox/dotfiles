4c460afdc2a54632d4b83287399be6a59576c227
Kelly Fox <kelly@bumfuddled.com>
2019-04-11 09:03:21 -0500

    make filenames more consistent

--- bin/bing-wallpaper.sh
+++ bin/bing-wallpaper.sh
@@ -4,18 +4,17 @@ PICTURE_DIR="$HOME/Google Drive/Pictures/Bing"
 
 mkdir -p "$PICTURE_DIR"
 
-urls=( "$(curl -s http://www.bing.com 2>/dev/null | \
+url="$(curl -s http://www.bing.com 2>/dev/null | \
   grep -Eo "url:'.*?'" | \
-  sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" -e "s/\\\//g")" )
+  head -1 |
+  sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" -e "s/\\\//g")"
 
 
-for p in "${urls[@]}"; do
-  filename=$(echo $p | sed 's/.*\/\(.*\)/\1/')
-  fullpath=$PICTURE_DIR/$filename
-  if [ ! -f "${fullpath}" ]; then
-    curl -s -Lo "$fullpath" "$p"
-    /usr/bin/osascript -e \
-      "tell application \"Finder\" to set desktop picture to POSIX file \"$fullpath\"" \
-      >/dev/null 2>&1
-  fi
-done
+filename=$(echo "$url" | sed 's/.*\/th.id=OHR.\([^&]*\)\&amp.*/\1/' )
+fullpath=$PICTURE_DIR/$filename
+if [ ! -f "${fullpath}" ]; then
+  curl -s -Lo "$fullpath" "$url"
+  /usr/bin/osascript -e \
+    "tell application \"Finder\" to set desktop picture to POSIX file \"$fullpath\"" \
+    >/dev/null 2>&1
+fi
